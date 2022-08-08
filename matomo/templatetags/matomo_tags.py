262601c426f190062a2b49970a9dcddad6572d86
EHKM (Muhammad Haseeb Kamal) <EHKM@novonordisk.com>
2022-08-08 14:17:35 +0200

    get user id from request object

--- matomo/templatetags/matomo_tags.py
+++ matomo/templatetags/matomo_tags.py
@@ -4,13 +4,16 @@
 from django import template
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-
+from django.http import request
 
 register = template.Library()
 
 
 @register.inclusion_tag('matomo/tracking_code.html')
 def tracking_code():
+    
+    user_id = request.user
+
     try:
         id = settings.MATOMO_SITE_ID
     except AttributeError:
@@ -19,4 +22,4 @@ def tracking_code():
         url = settings.MATOMO_URL
     except AttributeError:
         raise ImproperlyConfigured('MATOMO_URL does not exist.')
-    return {'id': id, 'url': url}
+    return {'id': id, 'url': url, 'user_id': user_id}
